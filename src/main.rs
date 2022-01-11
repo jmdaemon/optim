7da3fdc86870850962e4ea783d9ec802dc2cc0eb
Joseph Diza <josephm.diza@gmail.com>
2022-01-10 20:41:30 -0800

    Add cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Arg, App, ArgMatches};
+use std::path::{Path, PathBuf}; 
+
+use std::process::exit;
+
+fn getfp(opt: &str, matches: &ArgMatches) -> PathBuf {
+    let file = Path::new(matches.value_of(opt).unwrap_or(""));
+    if file.as_os_str() == "" { exit(1); }
+    file.to_path_buf()
 }
+
+fn main() {
+    let matches = App::new("Optim")
+        .version("0.1.0")
+        .author("Joseph Diza")
+        .about("Precisely optimizes the sizes of various images")
+        .arg(Arg::new("input").help("Input image file path"))
+        .arg(Arg::new("output").help("Output image file path"))
+        .get_matches();
+    let input: PathBuf = getfp("input", &matches);
+    let output: PathBuf = getfp("input", &matches);
+
+    println!("Input {}\nOutput {}", input.display(), output.display());
+}
+
