cbbf437aeb9ebe8befffa34d70b11349bff17403
Joseph Diza <josephm.diza@gmail.com>
2022-04-01 01:14:40 -0700

    Add logging libraries

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
-use clap::{Arg, App, ArgMatches};
 extern crate oxipng;
+use clap::{Arg, App, ArgMatches};
+use log::{debug, error, info};
 use std::path::{Path, PathBuf}; 
-
 use std::process::exit;
 
+/// Parse the cli args for a filepath
 fn getfp(opt: &str, matches: &ArgMatches) -> PathBuf {
     let file = Path::new(matches.value_of(opt).unwrap_or(""));
-    if file.as_os_str() == "" { exit(1); }
+    if file.as_os_str() == "" {
+        error!("Input file paths cannot be null");
+        exit(1);
+    }
     file.to_path_buf()
 }
 
-fn main() {
-    let matches = App::new("Optim")
+/// Create the command line interface
+fn build_cli() -> App<'static> {
+    let app = App::new("Optim")
         .version("0.1.0")
-        .author("Joseph Diza")
+        .author("Joseph Diza <josephm.diza@gmail.com>")
         .about("Precisely optimizes the sizes of various images")
         .arg(Arg::new("input").help("Input image file path"))
         .arg(Arg::new("output").help("Output image file path"))
-        .arg(Arg::new("optimizer").help("Optimizer to use"))
-        .get_matches();
+        .arg(Arg::new("optimizer").help("Optimizer to use"));
+    app
+}
+
+fn main() {
+    pretty_env_logger::init();
+    let matches = build_cli().get_matches();
 
     let input: PathBuf = getfp("input", &matches);
     let output: PathBuf = getfp("output", &matches);
     let optimizer = matches.value_of("optimizer").unwrap_or("oxipng");
 
-    println!("Input {}\nOutput {}\nOptimizer {}", input.display(), output.display(), optimizer);
+    debug!("Input {}\nOutput {}\nOptimizer {}", input.display(), output.display(), optimizer);
 
     match optimizer {
         //"oxipng" => {
@@ -33,7 +43,7 @@ fn main() {
             let outfile = oxipng::OutFile::Path(Some(output));
             let options = oxipng::Options::from_preset(4);
             let png = oxipng::optimize(&infile, &outfile, &options);
-            println!("{:?}\n", png);
+            info!("{:?}\n", png);
         }
     }
 
